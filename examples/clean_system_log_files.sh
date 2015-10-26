5d77cc5c77a626a079d9f45890f77db820b3359d
Alexandre Quessy <alexandre@quessy.net>
2015-10-26 14:55:12 -0400

    specify v4l2 device in lunch script only. add clean_system_log_files.sh

---
+++ examples/clean_system_log_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "" | sudo tee /var/log/syslog
+echo "" | sudo tee /var/log/kern.log
+
--- examples/toonloop-atelier-toonmap.sh
+++ examples/toonloop-atelier-toonmap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-toonloop --osc-send-port 17777 -d /dev/video1 --project-home ~/Documents/toonloop/atelier-toonmap
+toonloop --osc-send-port 17777 -d /dev/video0 --project-home ~/Documents/toonloop/atelier-toonmap
 # --width 800 --height 600
 
